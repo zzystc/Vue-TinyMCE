e82d5df9aee2e3f92709ed5e111c97e86f0b407d
zzy <104006706+zzystc@users.noreply.github.com>
2022-05-22 15:58:37 +0800

    add file

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# Vue-TinyMCE
- 基于Vue的TinyMCE富文本编辑器
+### 基于Vue的TinyMCE富文本编辑器
+- **技术栈：Vue2 、Vue CLI 和 TinyMCE**
+- **功能：在Vue中通过API自定义封装的富文本编辑器**
---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div >
+    <textarea id="mytextarea"></textarea>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  mounted() {
+    tinymce.init({
+      selector: '#mytextarea',
+      language: 'zh_CN',
+      branding: false,
+      toolbar: [
+        'undo redo | styleselect | bold italic | link image',
+        'alignleft aligncenter alignright hr'
+      ],
+      menu: {
+        file: {title: 'File', items: 'newdocument'},
+        edit: {title: 'Edit', items: 'undo redo | cut copy paste pastetext | selectall'},
+        insert: {title: 'Insert', items: 'link media | template hr'},
+        view: {title: 'View', items: 'visualaid'},
+        format: {title: 'Format', items: 'bold italic underline strikethrough superscript subscript | formats | removeformat'},
+        table: {title: 'Table', items: 'inserttable tableprops deletetable | cell row column'},
+        tools: {title: 'Tools', items: 'spellchecker code'},
+        plugins: "hr",
+        setup: function(editor) {
+          editor.on('click', function(e) {
+            console.log('Editor was clicked');
+          });
+        },
+        init_instance_callback : function(editor) {
+          console.log("Editor: " + editor.id + " is now initialized.");
+          }
+        }
+    });
+  },
+}
+</script>
